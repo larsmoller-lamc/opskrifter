d31439f13e791d197ddc0748595e830fe98a79d2
larsmoller-lamc <larsmollerchristensen@gmail.com>
2026-07-13 13:44:12 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 // Udskift alle værdier med dine egne fra Firebase Console
 // (Project settings → Your apps → Web app → SDK setup and configuration)
 window.firebaseConfig = {
-  apiKey: "DIN_API_KEY",
-  authDomain: "DIT_PROJEKT.firebaseapp.com",
-  projectId: "DIT_PROJEKT",
-  storageBucket: "DIT_PROJEKT.appspot.com",
-  messagingSenderId: "DIT_SENDER_ID",
-  appId: "DIT_APP_ID"
+  apiKey: "AIzaSyAfrMv1NykBeC-NaRQqr8Vh3DZdNYcaD7I",
+  authDomain: "opskrifter-badba.firebaseapp.com",
+  projectId: "opskrifter-badba",
+  storageBucket: "opskrifter-badba.firebasestorage.app",
+  messagingSenderId: "606026818328",
+  appId: "1:606026818328:web:1e55017db3c5581543746d"
 };
